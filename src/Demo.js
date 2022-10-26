e183881c38fac069eb57738d294b36c675206e37
beastRR <beastmd05@gmail.com>
2022-10-26 23:52:10 +0530

    first commit

---
+++ src/Demo.js
@@ -0,0 +1,19 @@
+import React from "react";
+import demo from "./rec/newDobDemo.jpg";
+import "./style.css";
+
+const Demo=()=>(
+    <div className=" main">
+        <div>
+            <div className="text-center box">
+                <img className="bg" src={demo} alt="demo" />
+            </div>
+        </div>
+        {/* <div>
+            <button></button>
+        </div> */}
+
+    </div>
+);
+
+export default Demo;
---
+++ src/Routing.js
@@ -0,0 +1,15 @@
+import React from "react";
+import App from "./App";
+import {BrowserRouter,Route, Routes} from "react-router-dom";
+import Demo from "./Demo";
+
+const Routing=()=>(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App/>}/>
+            <Route path="/web/index/auth/birthCertificate/view" element={<Demo/>}/>
+        </Routes>
+    </BrowserRouter>
+);
+
+export default Routing;
