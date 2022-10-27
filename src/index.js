3f0824eca6d0de62a1a00bfe1c299e89e0f003b8
beastRR <89104224+beastRR@users.noreply.github.com>
2022-10-27 21:46:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-// import App from './App';
-import Routing from './Routing';
-// import reportWebVitals from './reportWebVitals';
+import React from "react";
+import Routing from "./Routing";
+import ReactDom from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+const root = ReactDom.createRoot(document.getElementById("root"));  //we cant directly use ReactDom.render()...instead use ReactDom.createRoot(document.getElementById("root"))
 root.render(<Routing/>);
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
