04494c522e77cd6cae7c03ac317932ec24bff64c
beastRR <89104224+beastRR@users.noreply.github.com>
2022-11-03 21:22:18 +0530

    Update Routing.js

--- src/Routing.js
+++ src/Routing.js
@@ -2,7 +2,7 @@ import React from "react";
 // import App from "./App";
 import {BrowserRouter,Route, Routes} from "react-router-dom";
 import Demo from "./Demo";
-import B1 from "./rec/b1";
+import B1 from "./rec/B1";
 
 
 const Routing=()=>{
