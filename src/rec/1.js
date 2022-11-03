8cc7edafdc215d45ec2120c73920f1e834a323d8
beastRR <89104224+beastRR@users.noreply.github.com>
2022-11-03 20:56:11 +0530

    Create 1.js

---
+++ src/rec/1.js
@@ -0,0 +1,19 @@
+import React from "react";
+import shivam from "./newDobDemo.jpg";
+import "../style.css";
+
+const Demo=()=>(
+    <div className="text-center main">
+        <div>
+            <div className="text-center box">
+                <img className="bg" src={shivam} alt="data" />
+            </div>
+        </div>
+        {/* <div>
+            <button></button>
+        </div> */}
+
+    </div>
+);
+
+export default Demo;
