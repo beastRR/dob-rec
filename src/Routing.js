2345bf329c01c76f8ebe536415e0108f3ee92387
beastRR <89104224+beastRR@users.noreply.github.com>
2022-11-06 13:12:34 +0530

    Update Routing.js

--- src/Routing.js
+++ src/Routing.js
@@ -3,6 +3,8 @@ import React from "react";
 import {BrowserRouter,Route, Routes} from "react-router-dom";
 import Demo from "./Demo";
 import B1 from "./rec/B1";
+import B2 from "./rec/B2";
+
 
 
 const Routing=()=>{
@@ -13,8 +15,10 @@ const Routing=()=>{
         <Routes>
              <Route path="/web/index/auth/birthCertificate/view" element={<Demo/>}/>
                 {/*shivam*/}
-             <Route path="/web/index/auth/birthCertificate/view/B/bWi4lfhNgilpM8tZ" element={<b1/>}/>
              <Route path="/web/index/auth/birthCertificate/view/B/bWi4lfhNgilpM8tZ1" element={<B1/>}/> 
+                 
+                 {/*sumit*/}
+             <Route path="/web/index/auth/birthCertificate/view/B/aWio2rHfhNgilpM8tP1" element={<B2/>}/>
                  
                  
              
