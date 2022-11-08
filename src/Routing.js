d0eeb87393f394b1c0e8cd99b20c4bbc61bd0567
beastRR <beastmd05@gmail.com>
2022-11-08 13:28:12 +0530

    change B3 create b3

--- src/Routing.js
+++ src/Routing.js
@@ -4,6 +4,7 @@ import {BrowserRouter,Route, Routes} from "react-router-dom";
 import Demo from "./Demo";
 import B1 from "./rec/B1";
 import B2 from "./rec/B2";
+import B3 from "./rec/B3";
 
 
 
@@ -21,7 +22,7 @@ const Routing=()=>{
              <Route path="/web/index/auth/birthCertificate/view/B/aWio2rHfhNgilpM8tP1" element={<B2/>}/>
 
     {/*adiya*/}
-             <Route path="/web/index/auth/birthCertificate/view/B/aWio2rHfhNgilpM8tPqw" element={<B2/>}/>
+             <Route path="/web/index/auth/birthCertificate/view/B/bWio2rHfhNgilpM8tPqw" element={<B3/>}/>
                  
                  
              
---
+++ src/rec/B3.js
@@ -0,0 +1,19 @@
+import React from "react";
+import adi from "./B3.jpg";
+import "../style.css";
+
+const b3=()=>(
+    <div className="text-center main">
+        <div>
+            <div className="text-center box">
+                <img className="bg" src={adi} alt="data" />
+            </div>
+        </div>
+        {/* <div>
+            <button></button>
+        </div> */}
+
+    </div>
+);
+
+export default b3;
