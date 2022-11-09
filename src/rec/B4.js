8484a0a296806f51a114c464887016a087867a98
beastRR <89104224+beastRR@users.noreply.github.com>
2022-11-10 00:14:20 +0530

    Update B4.js

--- src/rec/B4.js
+++ src/rec/B4.js
@@ -1,1 +1,19 @@
-//he
+import React from "react";
+import SOU from "./B4.jpg";
+import "../style.css";
+
+const b4=()=>(
+    <div className="text-center main">
+        <div>
+            <div className="text-center box">
+                <img className="bg" src={SOU} alt="data" />
+            </div>
+        </div>
+        {/* <div>
+            <button></button>
+        </div> */}
+
+    </div>
+);
+
+export default b4;
