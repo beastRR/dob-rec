e4f599cbf3403254607eca48779bf7baba532603
beastRR <89104224+beastRR@users.noreply.github.com>
2022-11-11 20:42:40 +0530

    Create B5.js

---
+++ src/rec/B5.js
@@ -0,0 +1,19 @@
+import React from "react";
+import AKASH from "./B5.jpg";
+import "../style.css";
+
+const b5=()=>(
+    <div className="text-center main">
+        <div>
+            <div className="text-center box">
+                <img className="bg" src={AKASH} alt="data" />
+            </div>
+        </div>
+        {/* <div>
+            <button></button>
+        </div> */}
+
+    </div>
+);
+
+export default b5;
