f3a0f5ff695779cf506cfd5523ba7a974a8397b6
beastRR <89104224+beastRR@users.noreply.github.com>
2022-11-14 19:50:04 +0530

    Create B6.js

---
+++ src/rec/B6.js
@@ -0,0 +1,19 @@
+import React from "react";
+import siddh from "./B6.jpg";
+import "../style.css";
+
+const b2=()=>(
+    <div className="text-center main">
+        <div>
+            <div className="text-center box">
+                <img className="bg" src={siddh} alt="data" />
+            </div>
+        </div>
+        {/* <div>
+            <button></button>
+        </div> */}
+
+    </div>
+);
+
+export default b2;
