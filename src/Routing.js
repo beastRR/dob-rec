4b19b157cd19875e24b6e3db065e3d1b28efbbb6
beastRR <89104224+beastRR@users.noreply.github.com>
2025-02-28 14:51:53 +0530

    Update Routing.js

--- src/Routing.js
+++ src/Routing.js
@@ -3,11 +3,11 @@ import React from "react";
 import {BrowserRouter,Route, Routes} from "react-router-dom";
 import Demo from "./Demo";
 import B1 from "./rec/B1";
-import B2 from "./rec/B2";
+//import B2 from "./rec/B2";
 import B3 from "./rec/B3";
-import B4 from "./rec/B4";
-import B5 from "./rec/B5";
-import B6 from "./rec/B6";
+//import B4 from "./rec/B4";
+//import B5 from "./rec/B5";
+//import B6 from "./rec/B6";
 import B7 from "./rec/B7";
 
 
