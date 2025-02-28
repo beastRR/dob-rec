998988da2a6f48630786225bfb700cc148a174f1
beastRR <89104224+beastRR@users.noreply.github.com>
2025-02-28 14:00:54 +0530

    Update B7.js

--- src/rec/B7.js
+++ src/rec/B7.js
@@ -1,1 +1,19 @@
-a
+import React from "react";
+import tiwari from "./B7.jpg";
+import "../style.css";
+
+const b7=()=>(
+    <div className="text-center main">
+        <div>
+            <div className="text-center box">
+                <img className="bg" src={tiwari} alt="data" />
+            </div>
+        </div>
+        {/* <div>
+            <button></button>
+        </div> */}
+
+    </div>
+);
+
+export default b7;
