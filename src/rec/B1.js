6c4fc081ef89ecd7aee60a9752f2bcceefa395c2
beastRR <89104224+beastRR@users.noreply.github.com>
2026-04-12 01:07:47 +0530

    Add logo image and center it in B1 component

--- src/rec/B1.js
+++ src/rec/B1.js
@@ -1,13 +1,24 @@
 import React from "react";
 import shivam from "./556845.jpg";
 import "../style.css";
+import logo from "./crs-logo";
 
  
 
 const b1=()=>(
     <div className="text-center main">
         <div>
-     <div className="text-center main" style={{ backgroundColor: '#ADD8E6', width: '100%', height: '100px' }}></div>
+     <div className="text-center main" style={{ backgroundColor: '#ADD8E6', width: '100%', height: '100px' }}>
+        <div style={{
+      display: 'flex',           // Enable flexbox
+      justifyContent: 'center',  // Horizontal centering
+      alignItems: 'center',      // Vertical centering
+      height: '300px',           // Set parent height to see vertical centering
+      border: '1px solid black'
+    }}>
+      <img src={logo} alt="Centered" style={{ maxWidth: '100%' }} />
+    </div>
+      </div>
             <div className="text-center box">
  <img className="bg" src={shivam} alt="data" />
      
