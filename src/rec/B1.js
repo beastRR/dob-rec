09d05bd2865caba2460c1664d95348541608ab89
beastRR <89104224+beastRR@users.noreply.github.com>
2026-04-12 01:19:59 +0530

    Update B1.js

--- src/rec/B1.js
+++ src/rec/B1.js
@@ -10,7 +10,7 @@ const b1=()=>(
         <div>
      <div className="text-center main" style={{ display: 'flex',  backgroundColor: '#ADD8E6',          // Enable flexbox
       justifyContent: 'center',  // Horizontal centering
-      alignItems: 'center',, width: '100%', height: '100px' }}>
+      alignItems: 'center', width: '100%', height: '100px' }}>
 
         <img src={logo} alt="Centered" style={{ maxWidth: '100%' }} />
 {/*<div style={{
