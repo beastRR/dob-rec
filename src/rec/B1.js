9c1f9ef26a3341c5be5fb1cb35b94de23da4bbf7
beastRR <89104224+beastRR@users.noreply.github.com>
2026-04-12 01:18:58 +0530

    Refactor layout to use flexbox for centering
    Updated the layout to use flexbox for centering the content.

--- src/rec/B1.js
+++ src/rec/B1.js
@@ -8,16 +8,20 @@ import logo from "./crs-logo.png";
 const b1=()=>(
     <div className="text-center main">
         <div>
-     <div className="text-center main" style={{ backgroundColor: '#ADD8E6', width: '100%', height: '100px' }}>
-        <div style={{
+     <div className="text-center main" style={{ display: 'flex',  backgroundColor: '#ADD8E6',          // Enable flexbox
+      justifyContent: 'center',  // Horizontal centering
+      alignItems: 'center',, width: '100%', height: '100px' }}>
+
+        <img src={logo} alt="Centered" style={{ maxWidth: '100%' }} />
+{/*<div style={{
       display: 'flex',  backgroundColor: '#ADD8E6',          // Enable flexbox
       justifyContent: 'center',  // Horizontal centering
       alignItems: 'center',      // Vertical centering
       height: '300px',           // Set parent height to see vertical centering
       border: '1px solid black'
     }}>
-      <img src={logo} alt="Centered" style={{ maxWidth: '100%' }} />
-    </div>
+      
+    </div>/*}
       </div><p/>
             <div className="text-center box">
  <img className="bg" src={shivam} alt="data" />
