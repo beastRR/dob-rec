57dc27352788e94ed285a1d38ab4dce1aa5c1f9c
beastRR <89104224+beastRR@users.noreply.github.com>
2026-04-12 00:28:51 +0530

    Replace image with PDF embed in B1.js
    Comment out image import and add PDF embed.

--- src/rec/B1.js
+++ src/rec/B1.js
@@ -1,12 +1,18 @@
 import React from "react";
-import shivam from "./B1.jpg";
+// import shivam from "./B1.jpg";
 import "../style.css";
 
 const b1=()=>(
     <div className="text-center main">
         <div>
             <div className="text-center box">
-                <img className="bg" src={shivam} alt="data" />
+                // <img className="bg" src={shivam} alt="data" />
+     <embed
+        src="/556845.pdf"
+        type="application/pdf"
+        width="100%"
+        height="100%"
+      />
             </div>
         </div>
         {/* <div>
