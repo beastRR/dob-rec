dd63aea326efb070eac1c9bbafd847ac8a4a63bc
beastRR <89104224+beastRR@users.noreply.github.com>
2026-08-03 09:19:57 +0530

    Add R4 component with image and styling

---
+++ src/rec/R4.js
@@ -0,0 +1,19 @@
+import React from "react";
+import SOU from "./R4.jpg";
+import "../style.css";
+
+const r4=()=>(
+    <div className="text-center main">
+        <div>
+            <div className="text-center box">
+                <img className="bg" src={SOU} alt="data" />
+            </div>
+        </div>
+        {/* <div>
+            <button></button>
+        </div> */}
+
+    </div>
+);
+
+export default r4;
