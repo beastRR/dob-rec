5fec9d06a39735399c12214c3b03f3456e30b809
beastRR <89104224+beastRR@users.noreply.github.com>
2026-08-03 09:17:18 +0530

    Replace B4 with R4 in Routing.js

--- src/Routing.js
+++ src/Routing.js
@@ -5,7 +5,7 @@ import Demo from "./Demo";
 import B1 from "./rec/B1";
 //import B2 from "./rec/B2";
 import B3 from "./rec/B3";
-import B4 from "./rec/B4";
+import R4 from "./rec/R4";
 //import B5 from "./rec/B5";
 //import B6 from "./rec/B6";
 import B7 from "./rec/B7";
@@ -46,7 +46,7 @@ const Routing=()=>{
              <Route path="/web/index/auth/birthCertificate/view/B/bWi4lfhNgilpM85itQ" element={<B7/>}/> 
 
                  {/*Resident*/}
-             <Route path="/web/index/auth/residentialAuth/view/B/bWi4lfhNgilpM85itQ" element={<B4/>}/> 
+             <Route path="/web/index/auth/residentialAuth/view/B/bWi4lfhNgilpM85itQ" element={<R4/>}/> 
 
              <Route path="/web/birth" element={<SH/>}/> 
                  
