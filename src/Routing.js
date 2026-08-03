58b2dd90880643e4c915da5976fca5a9d380fc3b
beastRR <89104224+beastRR@users.noreply.github.com>
2026-08-03 08:59:38 +0530

    Update Routing.js

--- src/Routing.js
+++ src/Routing.js
@@ -5,7 +5,7 @@ import Demo from "./Demo";
 import B1 from "./rec/B1";
 //import B2 from "./rec/B2";
 import B3 from "./rec/B3";
-//import B4 from "./rec/B4";
+import B4 from "./rec/B4";
 //import B5 from "./rec/B5";
 //import B6 from "./rec/B6";
 import B7 from "./rec/B7";
