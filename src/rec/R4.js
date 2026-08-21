8766404fe62a9888745af9ab8d9ab5a7ca0c98a6
beastRR <89104224+beastRR@users.noreply.github.com>
2026-08-21 09:27:48 +0530

    Fix img tag syntax in R4.js

--- src/rec/R4.js
+++ src/rec/R4.js
@@ -6,7 +6,7 @@ import service from "./crs-logo.png";
 const r4=()=>(
     <div>
     <div style={{ width:'50%' , height:'50%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <img src={service} alt="Centered"  style={{width:'40%', height:'40%'/>
+      <img src={service} alt="Centered"  style={{width:'40%', height:'40%'}}/>
     </div>
         <div>
             <div className="text-center box">
