f2ac4d1c58c457dba634ab0782b42d7aa2c8298f
beastRR <89104224+beastRR@users.noreply.github.com>
2026-08-21 08:29:00 +0530

    Include service logo in R4 component
    Added a new image service logo to the R4 component.

--- src/rec/R4.js
+++ src/rec/R4.js
@@ -1,9 +1,13 @@
 import React from "react";
 import SOU from "./R9.jpg";
 import "../style.css";
+import service from "./crs-logo.png";
 
 const r4=()=>(
     <div className="text-center main">
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <img src={service} alt="Centered" />
+    </div>
         <div>
             <div className="text-center box">
                 <img className="bg" src={SOU} alt="data" />
