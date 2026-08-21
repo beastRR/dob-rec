37c7c3a70456dc2c7299009b676ccee386bbc0dc
beastRR <89104224+beastRR@users.noreply.github.com>
2026-08-21 09:44:29 +0530

    Adjust image height in R4 component
    Updated image height in R4 component for better layout.

--- src/rec/R4.js
+++ src/rec/R4.js
@@ -6,7 +6,7 @@ import service from "./crs-logo.png";
 const r4=()=>(
     <div>
     <div style={{ width:'50%' , height:'40px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <img src={service} alt="Centered"  style={{width:'40%', height:'50%'}}/>
+      <img src={service} alt="Centered"  style={{width:'40%', height:'40%'}}/>
     </div>
         <div>
             <div className="text-center box">
